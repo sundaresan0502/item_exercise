8cad72c1daaebf74851eb62933f9411e616624c3
Sundaresan <sundaresan0502@gmail.com>
2021-03-09 19:54:05 +0530

    item exercise commit

---
+++ src/ItemExample.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import model.Items;
+
+
+/**
+ * @author sundar
+ *
+ */
+public class ItemExample {
+	
+	public static void main(String[] args) {
+		List<Items> resultList;
+	    List<Items> itemList = new ArrayList<>();
+		itemList.add(new Items(101, "Ted", "Creative", true, 10000));
+		itemList.add(new Items(107, "Nexus", "Smartphone", true, 20000));
+		itemList.add(new Items(108, "Timex", "watch", true, 70000));
+		itemList.add(new Items(108, "Apple Watch", "Apple", false, 9000));
+		itemList.add(new Items(110, "Torino", "Laptop", true, 90000));
+		itemList.add(new Items(115, "Dell", "Laptop", true, 7000));
+		itemList.add(new Items(120, "Samsung", "Smatphone", false, 100000));
+		itemList.add(new Items(127, "Lenovo", "Tablet", true, 120000));
+		itemList.add(new Items(105, "Think", "Tablet", false, 4000));
+		itemList.add(new Items(109, "Sony Vaio", "Laptop", false, 8000));
+		itemList.add(new Items(102, "Oneplus", "smartphone", true, 33000));
+		resultList = itemList.stream().filter(x -> x.getBrand().startsWith("T")).sorted(Comparator.comparing(Items::getPrice).reversed())
+		.collect(Collectors.toList());
+		resultList.stream().forEach(System.out::println);
+	}
+
+}
---
+++ src/model/Items.java
@@ -0,0 +1,73 @@
+package model;
+
+/**
+ * @author sundar
+ *
+ */
+public class Items {
+
+	public Items(int itemid, String brand, String desc, boolean availablity, int price) {
+		super();
+		this.itemid = itemid;
+		this.brand = brand;
+		this.desc = desc;
+		this.availablity = availablity;
+		this.price = price;
+	}
+
+	private int itemid;
+
+	private String brand;
+
+	private String desc;
+
+	private boolean availablity;
+
+	private int price;
+
+	public int getItemid() {
+		return itemid;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public boolean isAvailablity() {
+		return availablity;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setItemid(int itemid) {
+		this.itemid = itemid;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public void setAvailablity(boolean availablity) {
+		this.availablity = availablity;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Items [itemid=" + itemid + ", brand=" + brand + ", desc=" + desc + ", availablity=" + availablity
+				+ ", price=" + price + "]\n";
+	}
+}
